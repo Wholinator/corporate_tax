b52ec06128479362824f9b7667a4d973a7adbef0
mwholey <mwholey@expediagroup.com>
2020-12-05 09:39:55 -0600

    Cleaned up code

--- graph.py
+++ graph.py
@@ -3,50 +3,75 @@ import csv
 
 tax_file = open('corporate_tax_rate.csv')
 president_file = open('presidents.csv')
+unemployment_file = open('unemployment_rate.csv', encoding='utf-8-sig')
 
-csvr = csv.reader(tax_file)
-presidents = csv.DictReader(president_file)
-
-president_dict = {}
-
-for year in presidents:
-  president_dict[int(year['year'])] = {
-    'name': year['name'],
-    'party': year['alignment']
-  }
-
-print(president_dict)
-
-dates = []
-rates = []
-years = [i for i in range(1912, 2020, 4)]
-years.insert(0, 1909)
-xticks = []
-
-for x in years:
-  if x % 4 != 0:
-    x = x - (x % 4)
-
-  name = president_dict[x]['name']
-
-  x = '{} ({})'.format(name, x).rjust(28)
-  xticks.append(x)
-
-header = next(csvr)
-for row in csvr:
-  date = float(row[0])
-  rate = float(row[1])
-
-  dates.append(date)
-  rates.append(rate)
-
-tax_range = [0, max(rates)]
+tax_csvr = csv.DictReader(tax_file)
+presidents_csvr = csv.DictReader(president_file)
+unemployment_csvr = csv.DictReader(unemployment_file)
 
 repredback = '#FCECE9' 
 dembluback = '#EAF0F9' 
 repred = '#FE5C40'
 demblu = '#3571C0'
 
+def get_unemployment(csvr):
+  data = {}
+  for row in csvr:
+    year = row['year']
+    rate = row['12']
+    data[year] = rate
+
+  return data
+
+def get_presidents(csvr):
+  president_dict = {}
+  for year in csvr:
+    president_dict[int(year['year'])] = {
+      'name': year['name'],
+      'party': year['alignment']
+    }
+
+  return president_dict
+
+def get_tax(csvr):
+  dates, rates = [[],[]]
+  for row in csvr:
+    date = float(row['year'])
+    rate = float(row['max_rate'])
+
+    dates.append(date)
+    rates.append(rate)
+
+  return dates, rates
+
+def get_years():
+  years = [i for i in range(1912, 2020, 4)]
+  years.insert(0, 1909)
+  return years
+
+def get_xticks(years, presidents):
+
+  xticks = []
+
+  for x in years:
+    if x % 4 != 0:
+      x = x - (x % 4)
+
+    name = presidents[x]['name']
+
+    x = '{} ({})'.format(name, x).rjust(28)
+    xticks.append(x)
+
+  return xticks
+
+years = get_years()
+dates, rates = get_tax(tax_csvr)
+presidents = get_presidents(presidents_csvr)
+
+xticks = get_xticks(years, presidents)
+
+
+tax_range = [0, max(rates)]
 
 fig, ax = plt.subplots()
 
@@ -54,10 +79,10 @@ ax.plot(dates, rates, color='black')
 plt.xticks(years, xticks, rotation=70, horizontalalignment='right', verticalalignment='top')
 plt.subplots_adjust(bottom=0.4, right=0.95)
 
-for pres in president_dict.keys():
+for pres in presidents.keys():
   start = pres
   end = pres + 4
-  color = repred if president_dict[pres]['party'] == 'Republican' else demblu
+  color = repred if presidents[pres]['party'] == 'Republican' else demblu
   plt.fill_betweenx(tax_range, start, end, color=color)
 
 
